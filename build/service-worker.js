f5da36362cbbde2ae57904633f9a4d87f614a151
WindBlade <vn.chemgio@yahoo.com>
2019-11-10 17:30:52 +0700

    submit new build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4ea32c28928c741345052b91f8906d12.js"
+  "/precache-manifest.56b0d1985093b8e33aca753f55acef66.js"
 );
 
 self.addEventListener('message', (event) => {
