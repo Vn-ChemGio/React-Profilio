e625a4d1095f251181f0247da6e7b96813dcf54c
WindBlade <vn.chemgio@yahoo.com>
2019-11-10 11:37:50 +0700

    remove jquery require in progressbar Skill

--- src/components/pages/Skill.js
+++ src/components/pages/Skill.js
@@ -1,8 +1,6 @@
 import React, {Component} from 'react';
 import Knob from "../Knob";
 
-import $ from 'jquery';
-
 class Skill extends Component {
     constructor(props) {
         super(props);
@@ -12,29 +10,52 @@ class Skill extends Component {
                 value1: 0,
                 value2: 0,
                 value3: 0
+            },
+            Progress:{
+                value1: 0,
+                value2: 0,
+                value3: 0,
+                value4: 0
             }
         };
-        this.Knob = {
-            input1: 90, input2: 80, input3: 75
+        this.Input= {
+            Knob : {
+                input1: 90, input2: 80, input3: 75
+            },
+            Progress: {
+                input1: 90, input2: 80, input3: 75, input4:60
+            }
         };
 
+
         setInterval(() => {
-            let {value1, value2, value3, update} = this.getNextValue(this.state.Knob);
+            let {value1, value2, value3, update} = Skill.getNextValue(this.state.Knob, this.Input.Knob);
             if (update) {
                 this.setState({
                     Knob: {value1, value2, value3}
                 })
             }
 
-            //TODO: clear Inteval
-        }, 10)
+            //TODO: clear Interval
+        }, 10);
+
+        setInterval(() => {
+            let {update, value1, value2, value3, value4} = Skill.getNextValue(this.state.Progress, this.Input.Progress);
+            if (update) {
+                this.setState({
+                    Progress: {value1, value2, value3, value4}
+                })
+            }
+
+            //TODO: clear Interval
+        }, 10);
 
     }
 
-    getNextValue(Knob) {
+    static getNextValue(Value, Input) {
         let update = false;
-        let {value1, value2, value3} = Knob;
-        let {input1, input2, input3} = this.Knob;
+        let {value1, value2, value3, value4} = Value;
+        let {input1, input2, input3, input4} = Input;
 
         if (value1 < input1) {
             value1++;
@@ -50,7 +71,12 @@ class Skill extends Component {
             update = true;
         }
 
-        return ({value1, value2, value3, update})
+        if(value4!=undefined && value4<input4){
+            value4++;
+            update = true;
+        }
+
+        return ({update, value1, value2, value3, value4})
     }
 
     render() {
@@ -63,36 +89,36 @@ class Skill extends Component {
                             <h4>Technical Skills</h4>
                             <label className="progress-bar-label">Wordpress</label>
                             <div className="progress">
-                                <div className="progress-bar" role="progressbar" aria-valuenow={85}
-                                     aria-valuemin={0} aria-valuemax={100}>
-                                    <span>85%</span>
+                                <div className="progress-bar" role="progressbar" aria-valuenow={this.state.Progress.value1}
+                                     aria-valuemin={0} aria-valuemax={100} style={{width:`${this.state.Progress.value1}%`}}>
+                                    <span>{this.state.Progress.value1}%</span>
                                 </div>
                                 {/* end .progress-bar */}
                             </div>
                             {/* end .progress */}
                             <label className="progress-bar-label">Photoshop</label>
                             <div className="progress">
-                                <div className="progress-bar" role="progressbar" aria-valuenow={99}
-                                     aria-valuemin={0} aria-valuemax={100}>
-                                    <span>99%</span>
+                                <div className="progress-bar" role="progressbar" aria-valuenow={this.state.Progress.value2}
+                                     aria-valuemin={0} aria-valuemax={100} style={{width:`${this.state.Progress.value2}%`}}>
+                                    <span>{this.state.Progress.value2}%</span>
                                 </div>
                                 {/* end .progress-bar */}
                             </div>
                             {/* end .progress */}
                             <label className="progress-bar-label">HTML&amp;CSS</label>
                             <div className="progress">
-                                <div className="progress-bar" role="progressbar" aria-valuenow={75}
-                                     aria-valuemin={0} aria-valuemax={100}>
-                                    <span>75%</span>
+                                <div className="progress-bar" role="progressbar" aria-valuenow={this.state.Progress.value3}
+                                     aria-valuemin={0} aria-valuemax={100} style={{width:`${this.state.Progress.value3}%`}}>
+                                    <span>{this.state.Progress.value3}%</span>
                                 </div>
                                 {/* end .progress-bar */}
                             </div>
                             {/* end .progress */}
                             <label className="progress-bar-label">Javascript</label>
                             <div className="progress">
-                                <div className="progress-bar" role="progressbar" aria-valuenow={67}
-                                     aria-valuemin={0} aria-valuemax={100}>
-                                    <span>67%</span>
+                                <div className="progress-bar" role="progressbar" aria-valuenow={this.state.Progress.value4}
+                                     aria-valuemin={0} aria-valuemax={100} style={{width:`${this.state.Progress.value4}%`}}>
+                                    <span>{this.state.Progress.value4}%</span>
                                 </div>
                                 {/* end .progress-bar */}
                             </div>
@@ -194,6 +220,7 @@ class Skill extends Component {
                                             inputColor='#232323'
                                             fgColor="#7c4dff"
                                             bgColor='#e8e8e8'
+
                                         />
 
                                     </div>
@@ -211,12 +238,6 @@ class Skill extends Component {
                 {/* end .container */}
             </section>
         )
-    }
-
-    componentDidMount() {
-        $('.progress-bar').css('width', function () {
-            return ($(this).attr('aria-valuenow') + '%');
-        });
     }
 }
 
